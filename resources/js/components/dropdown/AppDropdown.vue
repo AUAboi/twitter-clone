f8f8f23a2462659aee1bc7cf0ae124b0ddfa7bfd
Ahsan AUA <auawdigital@gmail.com>
2021-03-22 13:44:09 +0500

    dropdown structuring

---
+++ resources/js/components/dropdown/AppDropdown.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<div @click="open = !open">
+			<slot name="trigger" />
+		</div>
+		<div
+			v-show="open"
+			class="absolute bg-gray-900 z-50 rounded-lg w-56 overflow-hidden"
+		>
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "AppDropdown",
+	data() {
+		return {
+			open: false
+		};
+	}
+};
+</script>
+
+<style>
+</style>
--- resources/js/components/tweets/actions/AppTweetRetweetAction.vue
+++ resources/js/components/tweets/actions/AppTweetRetweetAction.vue
@@ -1,6 +1,13 @@
 <template>
 	<a href="" class="text-gray-600 flex items-center text-base">
-		<i class="fas fa-retweet mx-2"></i>
-		<span>0</span>
+		<AppDropdown>
+			<template slot="trigger">
+				<i class="fas fa-retweet mx-2"></i>
+				<span>0</span>
+			</template>
+
+			<AppDropdownItem />
+			<AppDropdownItem />
+		</AppDropdown>
 	</a>
 </template>
