a8f535f1b32c3d0bc0df4af3fee52bab7bd49252
Ahsan AUA <auawdigital@gmail.com>
2021-03-25 00:06:40 +0500

    load ui indicator for quotes

--- app/Http/Resources/TweetCollection.php
+++ app/Http/Resources/TweetCollection.php
@@ -46,7 +46,7 @@ class TweetCollection extends ResourceCollection
         return $user->likes()
             ->whereIn(
                 'tweet_id',
-                $this->collection->pluck('id')
+                $this->collection->pluck('id')->merge($this->collection->pluck('original_tweet_id'))
             )
             ->pluck('tweet_id')
             ->toArray();
@@ -61,7 +61,7 @@ class TweetCollection extends ResourceCollection
         return $user->retweets()
             ->whereIn(
                 'original_tweet_id',
-                $this->collection->pluck('id')
+                $this->collection->pluck('id')->merge($this->collection->pluck('original_tweet_id'))
             )
             ->pluck('original_tweet_id')
             ->toArray();
